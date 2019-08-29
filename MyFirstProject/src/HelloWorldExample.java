2d57887d83218675ac2916f16086ee549224cf1d
Miles343 <james.potter.nz@gmail.com>
2019-08-29 23:03:28 +1200

    okay

--- MyFirstProject/src/HelloWorldExample.java
+++ MyFirstProject/src/HelloWorldExample.java
@@ -4,6 +4,7 @@ public class HelloWorldExample {
 	public static void main(String[] args) {
 		// TODO Different comment
 		System.out.println("Hello World");
+		System.out.println("Yes hello!");
 	}
 
 }
